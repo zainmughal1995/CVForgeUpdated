8a04c82f52c188b386f24cdccc798bbf47e5df40
Zain <mughalzain1995@gmail.com>
2026-02-25 15:56:09 +0500

    frontend setup for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // change later
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
